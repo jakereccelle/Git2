e9af7df805b8299946e583bf2f5d7d5eb6c81196
Jake Reccelle <jcr21@students.ptcollege.edu>
2019-02-07 17:11:39 -0500

    this is the second commit

---
+++ src/github2/GitHub2.java
@@ -0,0 +1,11 @@
+package github2;
+
+public class GitHub2 {
+
+    public static void main(String[] args) {
+        Reccelle();
+    }
+    public static void Reccelle(){
+        System.out.println("Jake Reccelle again!");
+    }
+}
